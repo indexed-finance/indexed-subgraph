213f838aaba2c83be99f9993706aee65449a32b5
dillon <dillonkellar@protonmail.com>
2020-08-29 16:14:08 -0500

    update

--- src/oracle-mapping.ts
+++ src/oracle-mapping.ts
@@ -3,25 +3,26 @@ import { Category, Token } from '../generated/schema';
 import { Address } from '@graphprotocol/graph-ts';
 
 export function handleNewCategory(event: CategoryAdded): void {
-  let category = new Category(event.params.categoryID.toHex());
+  let category = new Category(event.params.categoryID.toHexString());
   category.metadataHash = event.params.metadataHash;
   category.tokens = [];
   category.save();
 }
 
 export function handleTokenAdded(event: TokenAdded): void {
-  let categoryID = event.params.categoryID.toHex();
-  let tokenAddress = event.params.token.toHex();
+  let categoryID = event.params.categoryID.toHexString();
+  let tokenAddress = event.params.token.toHexString();
   let category = Category.load(categoryID);
   let token = new Token(tokenAddress);
   token.category = categoryID;
   token.save();
+  if (category.tokens == null) category.tokens = [];
   category.tokens.push(tokenAddress);
   category.save();
 }
 
 export function handleCategorySorted(event: CategorySorted): void {
-  let category = Category.load(event.params.categoryID.toHex());
+  let category = Category.load(event.params.categoryID.toHexString());
   let oracle = MarketOracle.bind(event.address);
   let tokens = oracle.getCategoryTokens(event.params.categoryID);
   category.tokens = [];
