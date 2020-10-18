ac56f135789752c48f031edc50959b3c7bcbec56
samgos <samuel.jj.gosling@gmail.com>
2020-10-18 05:09:19 +0100

    finalise structure for token eventing conditioning

--- src/token-mapping.ts
+++ src/token-mapping.ts
@@ -2,6 +2,44 @@ import { NdxContract, Transfer, DelegateChanged, DelegateVotesChanged } from '..
 import { DailyDistributionSnapshot } from '../generated/schema';
 import { BigInt, Bytes } from '@graphprotocol/graph-ts';
 
+const ONE_DAY = 86400000;
+const NA = '0x0000000000000000000000000000000000000000'
+
+export function handleDelegateChanged(event: DelegateChanged){
+  let timestamp = event.block.timestamp.toI32();
+  let snapshot = initialiseSnapshot(timestamp);
+  let contract = NdxContract.bind(event.address);
+  let votes = contract.getPriorVotes(event.params.delegator, event.block.number)
+
+  if(event.params.delegatee != event.params.delegator){
+    snapshot.delegated = snapshot.delegated.add(vote)
+
+    if(event.params.delegator == event.params.currentDelegate){
+      snapshot.active = snapshot.active.sub(votes)
+    } else if(event.params.currentDelegate == NA){
+      snapshot.inactive = snapshot.inactive.sub(votes)
+    }
+  } else if(event.params.delegatee != NA) {
+    snapshot.active = snapshot.active.add(votes)
+
+    if(event.params.currentDelegate == NA){
+      snapshot.inactive = snapshot.inactive.sub(votes)
+    } else if(event.params.delegator != event.params.currentDelegate){
+      snapshot.delegated = snapshot.delegated.sub(votes)
+    }
+  } else {
+    snapshot.inactive = snapshot.active.add(votes)
+
+    if(event.params.currentDelegate == event.params.delegator){
+      snapshot.active = snapshot.active.sub(votes)
+    } else {
+      snapshot.delegated = snapshot.delegated.sub(votes)
+    }
+  }
+
+  snapshot.save()
+}
+
 export function handleTransfer(event: Transfer){
   let sender = event.params.from.toHexString();
   let recipent = event.params.to.toHexString();
@@ -15,24 +53,31 @@ export function handleTransfer(event: Transfer){
     snapshot.active = snapshot.active.sub(value)
   } else if(senderDelegate == NA){
     snapshot.inactive.sub(value)
-  } else {
-    snapshot.delegated.sub(value)
-  }  if(recipentDelegate == address){
+  } if(recipentDelegate == address){
     snapshot.active = snapshot.active.add(value)
   } else if(recipentDelegate == NA){
     snapshot.inactive.sub(value)
-  } else {
-    snapshot.delegated.sub(value)
   }
 
   snapshot.save()
 }
 
 export function handleDelegateVoteChange(event: DelegateVotesChanged){
-  let contract = NdxContract.bind(event.address);
+  let timestamp = event.block.timestamp.toI32();
+  let snapshot = initialiseSnapshot(timestamp);
+  let difference = BigInt.fromI32(0);
 
+  if(event.params.oldVotes > event.params.newVotes){
+    difference = event.params.oldVotes.sub(event.params.newVotes)
+    difference = snapshot.delegated.sub(difference)
+  } else {
+    difference = event.params.newVotes.sub(event.params.oldVotes)
+    difference = snapshot.delegated.add(difference)
+  }
 
+  snapshot.save()
 }
+
 
 function initialiseSnapshot(timestamp: Int): DailyDistributionSnapshot {
   let eventTimestamp = new Date(timestamp * 1000)
@@ -42,7 +87,7 @@ function initialiseSnapshot(timestamp: Int): DailyDistributionSnapshot {
 
   let previousTimestamp = eventTimestamp
 
-  previousTimestamp.setDate(previousTimestamp.getDate() - 1)
+  previousTimestamp.setTime(previousTimestamp.getTime() - ONE_DAY)
 
   let newTimestamp = eventTimestamp.getTime() / 1000
   let oldTimestamp = previousTimestamp.getTime() / 1000
@@ -53,7 +98,6 @@ function initialiseSnapshot(timestamp: Int): DailyDistributionSnapshot {
     newSnapshot = new DailyDistributionSnapshot(newTimestamp)
 
     if(oldSnapshot == null){
-      newSnapshot = new DailyDistributionSnapshot(newTimestamp)
       newSnapshot.active = oldSnapshot.active
       newSnapshot.inactive = oldSnapshot.inactive
       newSnapshot.delegated = oldSnapshot.delegaed
