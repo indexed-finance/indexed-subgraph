0fe8795869356e2ac5d572b225a0228bc56d8498
samgos <samuel.jj.gosling@gmail.com>
2020-10-18 03:03:22 +0100

    proposal event conditioning

--- src/dao-mapping.ts
+++ src/dao-mapping.ts
@@ -1,10 +1,11 @@
 import { ProposalCreated, ProposalQueued, ProposalCanceled, ProposalExecuted } from '../generated/GovernorAlpha/GovernorAlpha';
 import { Proposal, Vote, State } from '../generated/schema';
-import { BigInt } from '@graphprotocol/graph-ts';
+import { BigInt, Bytes } from '@graphprotocol/graph-ts';
 
 export function createProposal(event: ProposalCreated) {
-  let proposal = new Proposal(event.params.metadataHash);
   let address = event.transaction.from.toHexString();
+  let proposal = new Proposal(event.params.id);
+  
   proposal.against = event.params.againstVotes;
   proposal.signatures = event.params.signatures;
   proposal.calldatas = event.params.calldatas;
@@ -15,4 +16,24 @@ export function createProposal(event: ProposalCreated) {
   proposal.state = State.Active;
   proposal.proposer = address;
   proposal.save();
+}
+
+export function cancelProposal(event: ProposalCanceled) {
+  manageProposal(event.params.id, event.transaction.hash.toHex(), State.Cancelled)
+}
+
+export function queueProposal(event: ProposalQueued) {
+  manageProposal(event.params.id, event.transaction.hash.toHex(), State.Queued)
+}
+
+export function executeProposal(event: ProposalCanceled) {
+  manageProposal(event.params.id, event.transaction.hash.toHex(), State.Executed)
+}
+
+function manageProposal(id: string, transactionHash: Bytes, state: State) {
+  let proposal = Proposal.load(id)
+
+  proposal.action = transactionHash
+  proposal.state = state
+  proposal.save()
 }
